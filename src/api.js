e6492b54367fe8a1548f46baf48ab909eae00ca3
Parick Cowan <patrick.w.cowan@gmail.com>
2019-03-14 16:33:17 -0500

    added initial addMovie function and connectivity

--- src/api.js
+++ src/api.js
@@ -1,6 +1,27 @@
-module.exports = {
-  getMovies: () => {
+
+const getMovies = () => {
     return fetch('/api/movies')
       .then(response => response.json());
-  }
+  };
+
+
+const addMovie = (title, rating, id) => {
+
+  const newMovie = {title: title, rating: rating, id: id};
+  const url = '/api/movies';
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newMovie),
+  };
+  fetch(url, options)
+      .then(() => console.log('Movie was added'))
+      .catch(() => console.log('error'));
 };
+
+
+
+
+export default {getMovies,addMovie}
