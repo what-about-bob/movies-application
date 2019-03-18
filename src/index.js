33e44a8d30b3742a37cb4b7273eac51a36628111
Sabita-Regmi <sabitaregmi3291@gmail.com>
2019-03-18 09:08:24 -0500

    delete functionality done,now edit part ramaining

--- src/index.js
+++ src/index.js
@@ -5,25 +5,38 @@
  */
 
 import movies from  './api.js';
+$(".loading").addClass('hidden');
+const getMovies = () => {
+  return fetch('/api/movies')
+      .then(response => response.json());
+
+};
+
 
 function showMovies() {
+  $(".loading").addClass('show').fadeOut(2000);
   $('#containerformovie').html('');
   movies.getMovies().then((movies) => {
     console.log('Here are all the movies:');
     movies.forEach(({title, rating, id}) => {
       console.log(`id#${id} - ${title} - rating: ${rating}`);
-      $("#containerformovie").append(`<div><div><ul><li>Id:${id}
+      $("#containerformovie").append(`<div class="card" ><div><ul class="list-group list-group-flush"><li>Id:${id}
                                 </li><li>title:${title}
                                 </li><li>rating:${rating}
                                 </li>
+                                <li>
+                                <button class="btn-warning delete"  value="${id}">DELETE</button></li>
                                 </ul></div></div>`);
+
+
     });
   }).catch((error) => {
     alert('Oh no! Something went wrong.\nCheck the console for details.');
     console.log(error);
   });
-
+  // $(".loading").addClass('show');
 }
+
 
   let title= " ";
   let rating=0;
@@ -31,16 +44,24 @@ function showMovies() {
 
 
   $("#addBtn").click(function(e){
+    $(".loading").addClass('show').fadeOut(2000);
     e.preventDefault();
+    // $(".loading").toggleClass('show');
     title=$("#moviename").val();
     rating=$("#rating").val();
-
+    // $(".loading").addClass('show');
     movies.addMovie(title,rating);
-     showMovies();
-  });
 
+
+     showMovies();
+
+
+  });
+// $(".loading").addClass('hidden');
   $('#showBtn').click(function () {
+
 showMovies()
+
   });
 
 
@@ -51,10 +72,13 @@ idValue = $("#deleteID").val();
   showMovies()
 
 });
-//
-// $("#editBtn").click(function(){
-//   idValue = $("#editId").val();
-//   movies.editMovie(idValue);
-//   showMovies()
-//
-// })
+$('#containerformovie').on('click', '.delete', function (event){
+  let deleteFilm = $(event.target).val();
+  // console.log(deleteID);
+  movies.deleteMovie(deleteFilm);
+showMovies();
+});
+
+
+
+
